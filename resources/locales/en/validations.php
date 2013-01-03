b5df170d2012573d8237005ff528de19346059b1
Miles Johnson <mileswjohnson@gmail.com>
2013-01-03 12:09:19 -0800

    Removed doc blocks

--- resources/locales/en/validations.php
+++ resources/locales/en/validations.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Titon: A PHP 5.4 Modular Framework
- *
- * @copyright	Copyright 2010, Titon
- * @link		http://github.com/titon
- * @license		http://opensource.org/licenses/bsd-license.php (BSD License)
- */
 
 return [
 	'currency' => '/^\$[0-9,]+(?:\.[0-9]{2})?$/',
--- resources/locales/nl/locale.php
+++ resources/locales/nl/locale.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Titon: A PHP 5.4 Modular Framework
- *
- * @copyright	Copyright 2010, Titon
- * @link		http://github.com/titon
- * @license		http://opensource.org/licenses/bsd-license.php (BSD License)
- */
 
 // Dutch (Standard)
 return [
