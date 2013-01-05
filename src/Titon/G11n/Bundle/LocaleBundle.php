46a1ceeae03350c04edbb7453cbad7978a4c22e5
Miles Johnson <mileswjohnson@gmail.com>
2013-01-04 17:08:15 -0800

    Moved LocaleBundle and MessageBundle to the G11n package

---
+++ src/Titon/G11n/Bundle/LocaleBundle.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\G11n\Bundle;
+
+use Titon\Io\Bundle\AbstractBundle;
+use Titon\Io\Reader\PhpReader;
+
+/**
+ * The LocaleBundle manages the loading of locale resources which contain locale specific configuration,
+ * validation rules (phone numbers, zip codes, etc), inflection rules (plurals, singulars, irregulars, etc)
+ * and formatting rules (dates, times, etc).
+ */
+class LocaleBundle extends AbstractBundle {
+
+	/**
+	 * Add the PhpReader for locale bundle reading.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function initialize() {
+		$this->addReader(new PhpReader());
+	}
+
+}
---
+++ src/Titon/G11n/Bundle/MessageBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\G11n\Bundle;
+
+use Titon\Io\Bundle\AbstractBundle;
+
+/**
+ * The MessageBundle manages the loading of message catalogs for localization.
+ */
+class MessageBundle extends AbstractBundle {
+
+}
