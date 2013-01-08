5a7408c7e03cc1e2180e5f46f04914ccc6ba2165
Miles Johnson <mileswjohnson@gmail.com>
2013-01-07 17:06:57 -0800

    Added tests for bundles

---
+++ tests/Titon/G11n/Bundle/LocaleBundleTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\G11n\Bundle;
+
+use Titon\G11n\Bundle\LocaleBundle;
+use Titon\Test\TestCase;
+
+/**
+ * Test class for Titon\G11n\Bundle\LocaleBundle.
+ */
+class LocaleBundleTest extends TestCase {
+
+	/**
+	 * Parent instance.
+	 *
+	 * @access public
+	 * @var \Titon\G11n\Bundle\LocaleBundle
+	 */
+	public $object;
+
+	/**
+	 * Setup bundles for specific conditions.
+	 */
+	public function setUp() {
+		$this->object = new LocaleBundle(['locale' => 'ex']);
+		$this->object->addLocation(TEMP_DIR . '/locales/{locale}/');
+	}
+
+	/**
+	 * Test that the locale meta data is parsed correctly.
+	 */
+	public function testGetLocale() {
+		$locale = $this->object->loadResource('locale');
+
+		$this->assertTrue(is_array($locale));
+		$this->assertArraysEqual([
+			'code' => 'ex',
+			'iso2' => 'ex',
+			'iso3' => 'exp',
+			'timezone' => '',
+			'title' => 'Example Parent',
+		], $locale);
+	}
+
+	/**
+	 * Test that the formatting rules are parsed correctly.
+	 */
+	public function testGetFormats() {
+		$formats = $this->object->loadResource('formats');
+
+		$this->assertTrue(is_array($formats));
+		$this->assertEquals([
+			'date' => 'ex',
+			'time' => 'ex',
+			'datetime' => 'ex',
+			'pluralForms' => 2,
+			'pluralRule' => function() { }
+		], $formats);
+	}
+
+	/**
+	 * Test that the inflection rules are parsed correctly.
+	 */
+	public function testGetInflections() {
+		$inflections = $this->object->loadResource('inflections');
+
+		$this->assertTrue(is_array($inflections));
+		$this->assertEquals([
+			'irregular' => ['ex' => 'irregular'],
+			'uninflected' => ['ex'],
+			'plural' => ['ex' => 'plural'],
+			'singular' => ['ex' => 'singular']
+		], $inflections);
+	}
+
+	/**
+	 * Test that the validation rules are parsed correctly.
+	 */
+	public function testGetValidations() {
+		$validations = $this->object->loadResource('validations');
+
+		$this->assertTrue(is_array($validations));
+		$this->assertEquals([
+			'phone' => 'ex',
+			'postalCode' => 'ex',
+			'ssn' => 'ex'
+		], $validations);
+	}
+
+}
---
+++ tests/Titon/G11n/Bundle/MessageBundleTest.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * @copyright	Copyright 2010-2013, The Titon Project
+ * @license		http://opensource.org/licenses/bsd-license.php
+ * @link		http://titon.io
+ */
+
+namespace Titon\G11n\Bundle;
+
+use Titon\G11n\Bundle\MessageBundle;
+use Titon\Io\Reader\PhpReader;
+use Titon\Io\Reader\IniReader;
+use Titon\Io\Reader\JsonReader;
+use Titon\Io\Reader\XmlReader;
+use Titon\Io\Reader\PoReader;
+
+/**
+ * Test class for Titon\G11n\Bundle\MessageBundle.
+ */
+class MessageBundleTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * Test that loading messages from php files work.
+	 */
+	public function testPhpBundles() {
+		$bundle = new MessageBundle(['bundle' => 'ex']);
+		$bundle->addReader(new PhpReader())->addLocation(TEMP_DIR . '/messages/{bundle}/');
+
+		$messages = $bundle->loadResource('default');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEquals(['titon', 'test', 'type', 'format'], array_keys($messages));
+		$this->assertEquals([
+			'titon' => 'Titon',
+			'test' => 'Test',
+			'type' => 'php',
+			'format' => '{0,number,integer} health, {1,number,integer} energy, {2,number} damage'
+		], $messages);
+
+		$messages = $bundle->loadResource('doesntExist');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEmpty($messages);
+	}
+
+	/**
+	 * Test that loading messages from ini files work.
+	 */
+	public function testIniBundles() {
+		$bundle = new MessageBundle(['bundle' => 'ex']);
+		$bundle->addReader(new IniReader())->addLocation(TEMP_DIR . '/messages/{bundle}/');
+
+		$messages = $bundle->loadResource('default');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEquals(['titon', 'test', 'type', 'format'], array_keys($messages));
+		$this->assertEquals([
+			'titon' => 'Titon',
+			'test' => 'Test',
+			'type' => 'ini',
+			'format' => '{0,number,integer} health, {1,number,integer} energy, {2,number} damage'
+		], $messages);
+
+		$messages = $bundle->loadResource('doesntExist');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEmpty($messages);
+	}
+
+	/**
+	 * Test that loading messages from json files work.
+	 */
+	public function testJsonBundles() {
+		$bundle = new MessageBundle(['bundle' => 'ex']);
+		$bundle->addReader(new JsonReader())->addLocation(TEMP_DIR . '/messages/{bundle}/');
+
+		$messages = $bundle->loadResource('default');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEquals(['titon', 'test', 'type', 'format'], array_keys($messages));
+		$this->assertEquals([
+			'titon' => 'Titon',
+			'test' => 'Test',
+			'type' => 'json',
+			'format' => '{0,number,integer} health, {1,number,integer} energy, {2,number} damage'
+		], $messages);
+
+		$messages = $bundle->loadResource('doesntExist');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEmpty($messages);
+	}
+
+	/**
+	 * Test that loading messages from xml files work.
+	 */
+	public function testXmlBundles() {
+		$bundle = new MessageBundle(['bundle' => 'ex']);
+		$bundle->addReader(new XmlReader())->addLocation(TEMP_DIR . '/messages/{bundle}/');
+
+		$messages = $bundle->loadResource('default');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEquals(['titon', 'test', 'type', 'format'], array_keys($messages));
+		$this->assertEquals([
+			'titon' => 'Titon',
+			'test' => 'Test',
+			'type' => 'xml',
+			'format' => '{0,number,integer} health, {1,number,integer} energy, {2,number} damage'
+		], $messages);
+
+		$messages = $bundle->loadResource('doesntExist');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEmpty($messages);
+	}
+
+	/**
+	 * Test that loading messages from xml files work.
+	 */
+	public function testPoBundles() {
+		$bundle = new MessageBundle(['bundle' => 'ex']);
+		$bundle->addReader(new PoReader())->addLocation(TEMP_DIR . '/messages/{bundle}/LC_MESSAGES/');
+
+		$messages = $bundle->loadResource('default');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEquals(['basic', 'multiline', 'plurals', 'context', 'format'], array_keys($messages));
+		$this->assertEquals([
+			'basic' => 'Basic message',
+			'multiline' => "Multiline message\nMore message here\nAnd more message again",
+			'plurals' => ['plural', 'plurals'],
+			'context' => 'Context message',
+			'format' => '{0,number,integer} health, {1,number,integer} energy, {2,number} damage'
+		], $messages);
+
+		$messages = $bundle->loadResource('doesntExist');
+
+		$this->assertTrue(is_array($messages));
+		$this->assertEmpty($messages);
+	}
+
+}
